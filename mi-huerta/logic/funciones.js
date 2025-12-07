bad2e41112542b1c4daf8b7e71ae476f97d3b3b3
Andrés Fernández Salaud <afernandezs.iesjc@gmail.com>
2025-12-07 20:04:05 +0100

    Ahora si que si acabado del todo!

--- mi-huerta/logic/funciones.js
+++ mi-huerta/logic/funciones.js
@@ -4,11 +4,13 @@ function procesar() {
 function llevarAlSoporte() {
   window.location.href = "soporte.php";
 }
-// Reloj en formato 24h
+// ! Reloj en formato 24h
 function updateClock() {
   const now = new Date();
+  // ? Formateo con ceros a la izquierda, usando padStart
   let hours = now.getHours().toString().padStart(2, "0");
   let minutes = now.getMinutes().toString().padStart(2, "0");
+  // ! Actualización del DOM, evitando manipulación directa del innerHTML
   document.getElementById("hours").textContent = hours;
   document.getElementById("minutes").textContent = minutes;
 
@@ -18,14 +20,16 @@ function updateClock() {
     month: "long",
     day: "numeric",
   };
+  // ? Formateo de la fecha en español, con la primera letra en mayúscula
   const fecha = now.toLocaleDateString("es-ES", options);
   document.getElementById("date").textContent =
+    // # Capitalizar la primera letra de la fecha
     fecha.charAt(0).toUpperCase() + fecha.slice(1);
 }
 updateClock();
 setInterval(updateClock, 1000);
 
-// Mensaje de éxito
+// ! Mensaje de éxito, que desaparece tras 2 segundos
 window.addEventListener("load", () => {
   const msg = document.getElementById("conexionMsg");
   if (msg) {
